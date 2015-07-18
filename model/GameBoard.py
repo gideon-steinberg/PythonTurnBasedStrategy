64c2e4ed7f4ec923dcc1821e81b7c972b483fa8c
gideon-steinberg <gideon_steinberg@hotmail.com>
2015-07-18 15:06:48 +1200

    populate GameBoard properly + comments

--- model/GameBoard.py
+++ model/GameBoard.py
@@ -1,15 +1,30 @@
-from model.Sprites.Creature import Creature
-from model.Sprites.BlankSprite import BlankSprite
+from model.Sprites.Player import Player
+from model.Sprites.Sprite import Sprite
+from Utils.Constants import Constants
 
 class GameBoard:
     
     def print_board(self):
+        ''' Print the board 
+            Print out the type of each item
+        '''
         for i in range(0, len(self.board)):
             for j in range(0, len(self.board[i])):
                 print self.board[i][j].get_type() + ",",
             print ""
 
-    
-    def __init__(self, width = 3, height = 4):
-        self.board = [[BlankSprite()] * width] * height
+    def __init__(self, width = Constants.DEFAULT_BOARD_WIDTH(),
+                 height = Constants.DEFAULT_BOARD_HEIGHT()):
+        self.board = []
+        
+        # populate the board
+        # _ is an "i don't care variable"
+        for _ in range(0,width):
+            array = []
+            for _ in range(0,height):
+                array.append(Sprite())
+            self.board.append(array)
+            
+        # insert the player
+        self.board[2][3] = Player()
         
