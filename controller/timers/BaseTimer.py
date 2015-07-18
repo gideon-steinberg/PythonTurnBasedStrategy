fd5af11dd1d692083893ef40fd9abd09245503b3
gideon-steinberg <gideon_steinberg@hotmail.com>
2015-07-18 16:55:45 +1200

    remove unneeded function

--- controller/timers/BaseTimer.py
+++ controller/timers/BaseTimer.py
@@ -2,7 +2,7 @@ class BaseTimer:
     def __init__(self, frame_root, interval):
         self.__frame_root = frame_root
         self.__interval = interval
-        self.setup_timer()
+        self.__timer_tick()
         
     def do_action(self):
         ''' method implemented in children '''
@@ -11,6 +11,3 @@ class BaseTimer:
     def __timer_tick(self):
         self.do_action()
         self.__frame_root.after(self.__interval, self.__timer_tick)
-    
-    def setup_timer(self):
-        self.__timer_tick()
