e6f747fcfdddaa7dd14f045e80f038b9cee01dc0
gideon-steinberg <gideon_steinberg@hotmail.com>
2015-07-18 15:22:48 +1200

    add sprite height and width constants
    calculate height and width based on board height and width.

--- Utils/Constants.py
+++ Utils/Constants.py
@@ -2,16 +2,24 @@ class Constants:
     
     @staticmethod
     def DEFAULT_BOARD_HEIGHT():
-        return 8
+        return 12
     
     @staticmethod
     def DEFAULT_BOARD_WIDTH():
-        return 10
+        return 20
     
     @staticmethod
     def DEFAULT_FRAME_WIDTH():
-        return 800
+        return Constants.DEFAULT_BOARD_WIDTH() * Constants.DEFAULT_SPRITE_WIDTH()
         
     @staticmethod
     def DEFAULT_FRAME_HEIGHT():
-        return 600
+        return Constants.DEFAULT_BOARD_HEIGHT() * Constants.DEFAULT_SPRITE_HEIGHT()
+        
+    @staticmethod
+    def DEFAULT_SPRITE_WIDTH():
+        return 50
+    
+    @staticmethod
+    def DEFAULT_SPRITE_HEIGHT():
+        return 40
