fe960592b939d2cada822b8bb15e8437ca1a3c66
gideon-steinberg <gideon_steinberg@hotmail.com>
2015-07-20 20:22:30 +1200

    remove 0,0 from MovementHelper

--- helper/MovementHelper.py
+++ helper/MovementHelper.py
@@ -4,9 +4,6 @@ class MovementHelper:
     def get_possible_movement_spaces(movement_range, x, y):
         possible_movement = []
         
-        # 0 is a bit weird
-        MovementHelper.__insert_item(possible_movement,x,y)
-        
         # maths incoming
         for distance in range(movement_range + 2):
             # go through all the possible angles in a circle
@@ -16,8 +13,9 @@ class MovementHelper:
                 x_increment = int(math.ceil(distance * math.cos(angle)))
                 y_increment = int(math.ceil(distance * math.sin(angle)))
                 
-                # if the x + y coords are too big ignore it
-                if math.fabs(x_increment) + math.fabs(y_increment) > movement_range:
+                # if the x + y coords are too big or zero ignore it
+                absolute_value = math.fabs(x_increment) + math.fabs(y_increment)
+                if  absolute_value > movement_range or absolute_value == 0 :
                     continue
                 
                 MovementHelper.__insert_item(possible_movement,  x_increment + x,  y_increment + y)          
